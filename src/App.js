e92114697be9b67373baf55378cbaae3461ea01f
Lokesh-102214 <quan655737@gmail.com>
2025-09-02 18:45:12 +0530

    Removing the footer and header

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
+
 import Overview from './pages/Overview';
 import Objectives from './pages/Objectives';
 import KeyFeatures from './pages/KeyFeatures';
-import Footer from './components/Footer';
+
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <main>
         <Overview />
         <Objectives />
         <KeyFeatures />
       </main>
-      <Footer />
     </div>
   );
 }
